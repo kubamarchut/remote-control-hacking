5f446baa690da384b14e8995a8c038846ea31d55
Kuba Marchut <kuba.marchut@gmail.com>
2023-04-23 16:03:17 +0200

    applying samll changes

--- demo-hilbert.py
+++ demo-hilbert.py
@@ -10,10 +10,11 @@ plt.plot(data, color='b', label='signal')
 
 env = np.abs(sigtool.hilbert(data))
 treshold = np.max(env)/2
-square_sig = (env > treshold) * np.max(env)
+square_sig = (env > treshold) #* np.max(env)
 
 plt.plot(env, color='r', label='env')
 plt.plot(square_sig, color='g', label='square')
+print(square_sig.where(1)[0])
 
 plt.legend()
 plt.show()
