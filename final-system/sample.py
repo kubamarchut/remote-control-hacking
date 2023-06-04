91920352d2c71fb4ee742b345421bc34194a9dbc
kadlub <kuba.kadlubowski@gmail.com>
2023-06-04 18:40:01 +0200

    poprawki2

--- final-system/sample.py
+++ final-system/sample.py
@@ -1,17 +1,19 @@
 import numpy as np
 from datetime import datetime
 from record import current_time
+
+
 def find_sync_symbol(data, start, lenght_of_pulse):
-    #sync is one HIGH and 31 ZERO
-    #one is already detected
-    position = start + int(1.5 * lenght_of_pulse);
+    # sync is one HIGH and 31 ZERO
+    # one is already detected
+    position = start + int(1.5 * lenght_of_pulse)
     isItSync = True
     for i in range(24):
         if position >= len(data):
             return False
         if data[position] != 0:
             return False
-        
+
         position += lenght_of_pulse
 
     if isItSync:
@@ -19,13 +21,14 @@ def find_sync_symbol(data, start, lenght_of_pulse):
         while data[end] == 0:
             if (end >= len(data)):
                 return False
-            end +=1
+            end += 1
 
         lenght_of_pulse = (end-start)/(31+1)
-    
+
     return isItSync, lenght_of_pulse
 
-def find_first_impulse(data, start = 0):
+
+def find_first_impulse(data, start=0):
     ones = np.where(data == 1)[0]
     if (start >= len(ones)):
         return False
@@ -35,7 +38,7 @@ def find_first_impulse(data, start = 0):
         lenght_of_conseq += 1
         if lenght_of_conseq >= len(ones):
             break
-    
+
     return lenght_of_conseq - start, ones[start]
 
 
@@ -47,37 +50,37 @@ def main(signal):
         if not type(find_first_impulse(signal, n)) == tuple:
             break
         length_of_pulse, position = find_first_impulse(signal, n)
-        
+
         if type(find_sync_symbol(signal, position, length_of_pulse)) == tuple:
-            print("timestamp:",str(current_time()), "sync found")
-           
-            length_of_pulse_synced = find_sync_symbol(signal, position, length_of_pulse)[1]
+            print("timestamp:", str(current_time()), "sync found")
+
+            length_of_pulse_synced = find_sync_symbol(
+                signal, position, length_of_pulse)[1]
             #print(length_of_pulse, length_of_pulse_synced)
             #length_of_pulse = (length_of_pulse_synced*31 + length_of_pulse)//32
             length_of_pulse = length_of_pulse_synced
             start = position+(32*length_of_pulse_synced)
-           
-            
-            start += 0.5*length_of_pulse
+
+            start += 0.5 * length_of_pulse
             sampled_data = ""
             for i in range(96):
-                index = int(start + (i*length_of_pulse_synced))
+                index = int(start + (i * length_of_pulse_synced))
                 if index >= len(signal):
                     return False
                 sampled_data += str(int(signal[index]))
-            
+
             sampled_data_list.append(sampled_data)
             break
 
         ones.append(position)
         n += int(length_of_pulse)
 
-
     for xc in ones:
-      
+
         pass
 
-    return sampled_data_list;
+    return sampled_data_list
+
 
 if __name__ == "__main__":
     sample_data = np.genfromtxt('../demodulated_signal.csv', delimiter=',')
--- final-system/send.py
+++ final-system/send.py
@@ -9,14 +9,15 @@ GPIO.setmode(GPIO.BOARD)
 transmit_pin = 37
 GPIO.setup(transmit_pin, GPIO.OUT)
 
-# Sekwencja bitow do wys³ania 1332675
-sequence2_on ='0001010001010101110000110' #gniazdko 2 w³¹czenie
-sequence1_on ='0001010001010101001100110' #gniazdko 1  
+# Sekwencja bitow do wysï¿½ania 1332675
+sequence2_on = '0001010001010101110000110'  # gniazdko 2 wlaczenie
+sequence1_on = '0001010001010101001100110'  # gniazdko 1
 sequence1_off = '0001010001010101001111000'
-# Ustawienie parametrów czasowych
+
+# Ustawienie parametrï¿½w czasowych
 zero_delay = 0.00016*0.66  # Dlugosc czasu trwania bitu 0
 one_delay = 0.0005*0.66   # Dlugosc czasu trwania bitu 1
-gap_zero = 0.00054*0.66  # Dlugosc czasu przerwy miêdzy bitami
+gap_zero = 0.00054*0.66  # Dlugosc czasu przerwy miedzy bitami
 gap_one = 0.0002*0.66
 gap_signal = 0.0053*0.66
 
@@ -27,28 +28,30 @@ def send_bit(bit):
         time.sleep(zero_delay)
         GPIO.output(transmit_pin, GPIO.LOW)
         time.sleep(gap_zero)
-        
+
     elif bit == '1':
         GPIO.output(transmit_pin, GPIO.HIGH)
         time.sleep(one_delay)
         GPIO.output(transmit_pin, GPIO.LOW)
         time.sleep(gap_one)
-        
+
+
 def sendMain(sequence):
-  i=0
-  while i<15:
-    for bit in sequence:
-      send_bit(bit)
-    time.sleep(gap_signal)
-    i+=1
-  GPIO.cleanup()
-    
+    i = 0
+    while i < 15:
+        for bit in sequence:
+            send_bit(bit)
+        time.sleep(gap_signal)
+        i += 1
+    GPIO.cleanup()
+
+
 if __name__ == "__main__":
-# Wysylanie sekwencji bitow
-  i=0
-  while i<15:
-    for bit in sequence1_off:
-      send_bit(bit)
-    time.sleep(gap_signal)
-    i+=1
-  GPIO.cleanup()
+    # Wysylanie sekwencji bitow
+    i = 0
+    while i < 15:
+        for bit in sequence1_off:
+            send_bit(bit)
+        time.sleep(gap_signal)
+        i += 1
+    GPIO.cleanup()
