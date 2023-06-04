4528dc2944254799ebacd38346a0e586d990ce35
kadlub <kuba.kadlubowski@gmail.com>
2023-06-04 16:30:44 +0200

    male poprawki

--- final-system/demodulate.py
+++ final-system/demodulate.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.signal import butter, lfilter, freqz, filtfilt
-
 import scipy.signal as sigtool
 
 plot = True
@@ -10,36 +9,42 @@ fs = 10
 cutoff = 0.75
 T = 5.0
 
+
 def rectifier(my_data):
     rect_data = (np.abs(my_data))
     return rect_data
 
+
 def butter_lowpass(cutoff, fs, order=5):
     return butter(order, cutoff, fs=fs, btype='low', analog=False)
+
 
 def butter_lowpass_filter(data, cutoff, fs, order=5):
     b, a = butter_lowpass(cutoff, fs, order=order)
     y = filtfilt(b, a, data)
     return y
 
+
 def comparator(y, data):
-  env = np.abs(data)
-  treshold = np.max(env) * 0.2899
-  square_sig = (y > treshold) * 1
-  return square_sig
+    env = np.abs(data)
+    treshold = np.max(env) * 0.2899
+    square_sig = (y > treshold) * 1
+    return square_sig
+
 
 def main(data):
-    n = len(data) # total number of samples
+    n = len(data)  # total number of samples
     b, a = butter_lowpass(cutoff, fs, order)
     data = rectifier(data)
     t = np.linspace(0, T, n, endpoint=False)
     y = butter_lowpass_filter(data, cutoff, fs, order)
-    #print(rectifier(data))
-   
+    # print(rectifier(data))
+
     square_signal = comparator(y, data)
 
     np.savetxt("../demodulated_signal.csv", square_signal, delimiter=",")
     return square_signal
+
 
 if __name__ == '__main__':
     sample_data = np.genfromtxt('../signal.csv', delimiter=',')
--- final-system/main.py
+++ final-system/main.py
@@ -9,53 +9,57 @@ from gpiozero import RGBLED
 from time import sleep
 from record import current_time
 
-  
 
 TRANSMIT_PIN = 37
 sequence1_off = '0001010001010101001111000'
 if __name__ == "__main__":
     button = Button("GPIO4")
-    button2 = Button("GPIO2") 
+    button2 = Button("GPIO2")
     led = RGBLED(red="GPIO22", green="GPIO23", blue="GPIO27")
     while True:
-      try:
-        led.color = (1, 0.5, 1)
-        
-        if button2.is_pressed:
-            #sendMain(sequence1_off)
-            #led.color = (1, 0, 0)
-            print("")
-          
-        if button.is_pressed:
-            led.color = (1, 0, 0)
-            print("timestamp:",  str(current_time()),"signal recording - start")
-            recorded_data = record()
-            print("timestamp:",  str(current_time()),"signal recording - end")          
-           
-            led.color = (1, 1, 0)
-            print("timestamp:",  str(current_time()),"decimating signal")
-            decimated_data = decimate(recorded_data)
+        try:
+            led.color = (1, 0.5, 1)
 
-            led.color = (0, 1, 1)
-            print("timestamp:", str(current_time()), "demodulating data" )
-            demodulated_data = demodulateMain(decimated_data)
+            if button2.is_pressed:
+                # sendMain(sequence1_off)
+                #led.color = (1, 0, 0)
+                print("")
 
-            led.color = (0, 0, 1)
-            print( "timestamp:", str(current_time()), "sampling signal" )
-            sampled_data = sampleMain(demodulated_data)
+            if button.is_pressed:
+                led.color = (1, 0, 0)
+                print("timestamp:",  str(current_time()),
+                      "signal recording - start")
+                recorded_data = record()
+                print("timestamp:",  str(current_time()),
+                      "signal recording - end")
 
-            if sampled_data:
-                led.color = (1, 0, 1)
-                print("timestamp:", str(current_time()), "decoding data" )
-                decoded_code = decodeMain(sampled_data[0])
-                
-                if decoded_code != False:
-                    led.blink(1, 1, 1, 1, (0, 1, 0), (0, 0, 0), 5)
-                    print("timestamp:", str(current_time()),"output:", decoded_code)
+                led.color = (1, 1, 0)
+                print("timestamp:",  str(current_time()), "decimating signal")
+                decimated_data = decimate(recorded_data)
+
+                led.color = (0, 1, 1)
+                print("timestamp:", str(current_time()), "demodulating data")
+                demodulated_data = demodulateMain(decimated_data)
+
+                led.color = (0, 0, 1)
+                print("timestamp:", str(current_time()), "sampling signal")
+                sampled_data = sampleMain(demodulated_data)
+
+                if sampled_data:
+                    led.color = (1, 0, 1)
+                    print("timestamp:", str(current_time()), "decoding data")
+                    decoded_code = decodeMain(sampled_data[0])
+
+                    if decoded_code != False:
+                        led.blink(1, 1, 1, 1, (0, 1, 0), (0, 0, 0), 5)
+                        print("timestamp:", str(current_time()),
+                              "output:", decoded_code)
+                    else:
+                        print("timestamp:", str(current_time()),
+                              "nie znaleziono kodu pilota")
                 else:
-                    print("timestamp:", str(current_time()),"nie znaleziono kodu pilota" )
-            else:
-                print("timestamp:", str(current_time()),"nie znaleziono kodu pilota")
-      except KeyboardInterrupt:
-        print("The Program is terminated manually")
-        raise SystemExit
+                    print("timestamp:", str(current_time()),
+                          "nie znaleziono kodu pilota")
+        except KeyboardInterrupt:
+            print("The Program is terminated manually")
+            raise SystemExit
